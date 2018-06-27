7e6d2e8212d3c13f2d43a064b3464b960c38c4a1
softjectico <40361465+softjectico@users.noreply.github.com>
2018-06-27 19:53:41 +0530

    Add files via upload

---
+++ PRIME.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i,q;
+	clrscr();
+	TOP:
+	printf("\nEnter a no :\n");
+	scanf("%d",&n);
+	for(i=2;i<=(n/2);i++)
+	{
+	  if(n%i==0)
+	  {
+		printf("No %d is not prime\n",n);
+		goto TOP;
+
+	  }
+	}
+	printf("No %d is prime \n",n);
+	printf("\nTo repeat the program repeat press 1 or press any key to exit : \t");
+	scanf("%d",&q);
+	if(q==1)
+	{
+		goto TOP;
+	}
+
+	getch();
+}
